368986e08e5b6d2b6cc926db9171d086ef925973
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2020-05-06 23:46:37 -0400

    Reeeebase
    Oh so that's what rebase means

--- judge/migrations/0106_partially_hide_scoreboard.py
+++ judge/migrations/0106_partially_hide_scoreboard.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0104_contestproblem_maxsubs'),
+        ('judge', '0105_webauthn'),
     ]
 
     operations = [
