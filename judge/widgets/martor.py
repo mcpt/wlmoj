3fc92bc1983b565f7a7faf7b3d8e2d238d289175
Jason <git@jasoncameron.dev>
2024-09-07 23:17:54 -0400

    fix linting issue in martor.py
    Signed-off-by: Jason <git@jasoncameron.dev>

--- judge/widgets/martor.py
+++ judge/widgets/martor.py
@@ -4,15 +4,16 @@ __all__ = ['MartorWidget', 'AdminMartorWidget']
 
 
 class MartorWidget(OldMartorWidget):
-    class Media:
-        css = {
-            'all': ['martor-description.css'],
-        }
-        js = ['martor-mathjax.js']
+	class Media:
+		css = {
+			'all': ['martor-description.css'],
+		}
+		js = ['martor-mathjax.js']
 
 
 class AdminMartorWidget(OldAdminMartorWidget):
-    UPLOADS_ENABLED = True
-    class Media:
-        css = MartorWidget.Media.css
-        js = ['admin/js/jquery.init.js', 'martor-mathjax.js']
+	UPLOADS_ENABLED = True
+	
+	class Media:
+		css = MartorWidget.Media.css
+		js = ['admin/js/jquery.init.js', 'martor-mathjax.js']
