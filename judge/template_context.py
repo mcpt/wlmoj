e3edd06e60695b5614ddafc578665db9fe841fe3
Jason Cameron <git@jasoncameron.dev>
2024-09-24 23:31:04 -0400

    chore: fix linting
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- judge/template_context.py
+++ judge/template_context.py
@@ -95,7 +95,7 @@ def site_theme(request):
 
 def math_setting(request):
     caniuse = CanIUse(request.META.get('HTTP_USER_AGENT', ''))
-    
+
     # Middleware populating `profile` may not have loaded at this point if we're called from an error context.
     if hasattr(request.user, 'profile'):
         engine = request.profile.math_engine
